d74ecc193ed744e6f39f90b77454b6a8d4ef9548
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-26 14:30:35 -0700

    init color constants
    just the basics -- avoided making any notable design decisions; probably will do a similar thing for the fonts

---
+++ src/constants/Colors.js
@@ -0,0 +1,37 @@
+export const white = '#FFFFF';
+export const gray = '#757575';
+export const black = '#050505';
+export const purple = '#A445ED';
+export const red = '#FF5252';
+
+const Themes = {
+  light: {
+    text: black,
+    // textMidContrast: '#4b6a9b',
+    // textLowContrast: '#697c9a',
+    // textSearchPlaceholder: '#4b6a9b',
+
+    background: white,
+    // backgroundSecondary: '#fefefe',
+
+    // primary: '#0079ff',
+    // primaryHover: '#60abff',
+
+    error: red,
+
+    tabBarActive: purple,
+    tabBarInactive: gray,
+  },
+  dark: {
+    text: white,
+
+    background: black,
+
+    error: red,
+
+    tabBarActive: purple,
+    tabBarInactive: gray,
+  },
+};
+
+export default Themes;
