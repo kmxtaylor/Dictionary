2419baa475aa8daa70c25e71d684003a35d1733d
Katie Taylor <katiemxtaylor@gmail.com>
2023-05-26 17:03:22 -0700

    load other fonts
    just loading them, not yet using them

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -4,28 +4,27 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
 import Tabs from 'navigation/Tabs';
 import { useTheme } from 'hooks/useTheme';
-// import { useThemeColors } from 'hooks/useThemeColors';
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
 const RootNavigation = () => {
-  // const { colors } = useThemeColors();
   const { theme, colors } = useTheme();
 
   const [fontsLoaded] = useFonts({
-    // 'Inconsolata-Regular': require('/../assets/fonts/inconsolata/static/Inconsolata-Regular.ttf'),
-    // 'Inconsolata-Bold': require('/../assets/fonts/inconsolata/static/Inconsolata-Bold.ttf'),
-    // 'Inconsolata-Bold': require('/../assets/fonts/inconsolata/Inconsolata-VariableFont_wdth,wght.ttf'),
+    'Inconsolata-Regular': require('/../assets/fonts/inconsolata/static/Inconsolata-Regular.ttf'),
+    'Inconsolata-Bold': require('/../assets/fonts/inconsolata/static/Inconsolata-Bold.ttf'),
     'Inter-Regular': require('/../assets/fonts/inter/static/Inter-Regular.ttf'),
     'Inter-Bold': require('/../assets/fonts/inter/static/Inter-Bold.ttf'),
-    // 'Inter': require('/../assets/fonts/inter/Inter-VariableFont_slnt,wght.ttf'), // Asset not found for android
+    'Lora-Regular': require('/../assets/fonts/lora/static/Lora-Regular.ttf'),
+    'Lora-Bold': require('/../assets/fonts/lora/static/Lora-Bold.ttf'),
+    'Lora-BoldItalic': require('/../assets/fonts/lora/static/Lora-BoldItalic.ttf'),
   });
 
   const navigationTheme = {
     ...DefaultTheme,
     colors: {
-      ...DefaultTheme.colors, // this is affecting the header background color, among other things
+      ...DefaultTheme.colors,
       ...colors,
       border: 'transparent',
     },
